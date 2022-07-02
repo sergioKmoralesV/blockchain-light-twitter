cd2f7c9e40e33c11bc279fd78cb6b099e0f2c224
Sergio Morales <s.mora.94@gmail.com>
2022-07-02 10:52:53 +0200

    Added the setup to prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 120,
+    tabWidth: 2,
+    singleQuote: false,
+    jsxSingleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    proseWrap: 'never',
+    htmlWhitespaceSensitivity: 'strict',
+    endOfLine: 'lf',
+}
