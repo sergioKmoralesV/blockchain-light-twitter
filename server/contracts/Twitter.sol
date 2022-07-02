8ca92c31027b60565ab4fb6a360594b15d7a2f97
Isaacgv <isaacgonzalesv@gmail.com>
2022-07-02 17:19:15 +0200

    add updateTweet and  readTweets functions #2

--- server/contracts/Twitter.sol
+++ server/contracts/Twitter.sol
@@ -5,6 +5,7 @@ contract Twitter {
 
     event CreateTweet(address recipient, uint tweetId);
     event DeleteTweet(uint tweetId, bool isDeleted);
+    event UpdateTweet(uint tweetId, string tweetText);
 
     struct Tweet {
         uint id;
@@ -33,11 +34,48 @@ contract Twitter {
         emit CreateTweet(msg.sender, tweetId);
     }
 
+
+    /**
+    * @dev Method to delete a tweet
+    * @param _tweetId The id of the tweet to be deleted
+     */
     function deleteTweet(uint _tweetId) external {
         require(tweetToOwner[_tweetId] == msg.sender, "You are not the owner");
         tweets[_tweetId].isDeleted = true;
         emit DeleteTweet(_tweetId, true);
     }
    
+    /**
+    * @dev Method to update a tweet
+    * @param _tweetId The id of the tweet to be updated
+    * @param _tweetText The new text of the tweet
+     */
+    function updateTweet(uint _tweetId, string memory _tweetText) external {
+        require(tweetToOwner[_tweetId] == msg.sender, "You are not the owner");
+        tweets[_tweetId].tweetText = _tweetText;
+        emit UpdateTweet(_tweetId, _tweetText);
+    }
+
+    /**
+    * @dev Method to get all the Tweets
+    * @return The array of Tweets
+    */
+    function readTweets() external view returns (Tweet[] memory) {
+        Tweet[] memory temporary = new Tweet[](tweets.length);
+        uint counter = 0;
+        for(uint i=0; i<tweets.length; i++) {
+            if(tweets[i].isDeleted == false) {
+                temporary[counter] = tweets[i];
+                counter++;
+            }
+        }
+
+        Tweet[] memory result = new Tweet[](counter);
+        for(uint i=0; i<counter; i++) {
+            result[i] = temporary[i];
+        }
+        return result;
+    }
+
  
 }
