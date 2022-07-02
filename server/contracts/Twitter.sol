de03d113547d96c3bb4fbbd205790180c4307fd3
Isaacgv <isaacgonzalesv@gmail.com>
2022-07-02 16:41:27 +0200

    add addTweet function

---
+++ server/contracts/Twitter.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+contract Twitter {
+
+    event AddTweet(address recipient, uint tweetId);
+
+    struct Tweet {
+        uint id;
+        address userName;
+        string tweetText;
+        bool isDeleted;
+    }
+    mapping(uint256 => address) tweetToOwner;
+    mapping(address => uint256) ownerTweetCount;
+
+
+    Tweet[] private tweets;
+
+    /**
+     * @dev Method to be called by our frontend when trying to add a new Tweet
+     * @param _tweetText The text of the tweet
+     * @param _isDeleted Whether the tweet is deleted or not
+     */
+
+    function _addTweet(string memory _tweetText, bool _isDeleted) external {
+        uint tweetId = tweets.length -1;
+        tweets.push(
+            Tweet(tweetId, msg.sender, _tweetText, _isDeleted));
+        tweetToOwner[tweetId] = msg.sender;
+        ownerTweetCount[msg.sender]++;
+        emit AddTweet(msg.sender, tweetId);
+    }
+
+ 
+}
